94b8bed7fa94ac6dcfb1e44ce8962bbde7da4564
camrex <camrex@rmimidwest.com>
2016-04-25 14:14:58 -0500

    Added database configuration to conf

--- conf.py
+++ conf.py
@@ -1,14 +1,18 @@
 
 #
 #
-# configuration file - contains customization for exact system
-# JCS 11/8/2013
+# WeatherPi configuration file - contains customization for exact system
+# 4/25/2016 
 #
 
-mailUser = "yourusename"
+# E-mail Configuration (gmail)
+mailUser = "yourusername"
 mailPassword = "yourmailpassword"
-
 notifyAddress ="you@example.com"
-
 fromAddress = "yourfromaddress@example.com"
 
+# Database Configuration
+DATABASEHOST = "localhost"
+DATABASENAME = "WeatherPi"              # MySQL Database Name for WeatherPi
+DATABASEUSER = "root"                   # MySQL user with access to WeatherPi DB
+DATABASEPASSWORD = "yourDBpassword"     # Password for MySQL user
