066b6323b681a6e7f18c7d07d13993e14a8f7456
camrex <camrex@rmimidwest.com>
2016-04-26 21:56:17 -0500

    fixed error thrown by systemctl in pclogging.py, etc

--- pclogging.py
+++ pclogging.py
@@ -7,14 +7,12 @@
 # contains logging data 
 #
 
-
 CRITICAL=50
 ERROR=40
 WARNING=30
 INFO=20
 DEBUG=10
 NOTSET=0
-
 
 import sys
 import time
@@ -30,42 +28,30 @@ def log(level, source, message):
 
    LOWESTDEBUG = 0
 	# open mysql database
-
 	# write log
-
-
 	# commit
-
-
 	# close
+
+   con = None
+   cur = None
 
    if (level >= LOWESTDEBUG):
         try:
-	
                 #print("trying database")
                 con = mdb.connect(conf.DATABASEHOST, conf.DATABASEUSER, conf.DATABASEPASSWORD, conf.DATABASENAME);
-
                 cur = con.cursor()
                 #print "before query"
-
                 query = "INSERT INTO systemlog(TimeStamp, Level, Source, Message) VALUES(UTC_TIMESTAMP(), %i, '%s', '%s')" % (level, source, message)
 	        #print("query=%s" % query)
-
                 cur.execute(query)
-
                 con.commit()
-
-
         except mdb.Error, e:
-
                 print "Error %d: %s" % (e.args[0],e.args[1])
-                con.rollback()
+                if con is not None: con.rollback()
                 #sys.exit(1)
-
         finally:
-                cur.close()
-                con.close()
-
+                if cur is not None: cur.close()
+                if con is not None: con.close()
                 del cur
                 del con
 
